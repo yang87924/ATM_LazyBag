b1ba002341015ea88cb0d664f94e443335925c96
yang <yang87924@gmail.com>
2023-04-13 22:14:03 +0800

    first commit

---
+++ fullend-spring-boot-react-crud/backend/src/main/java/io/mahesh/backend/User.java
@@ -0,0 +1,20 @@
+package io.mahesh.backend;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String idNumber;
+    private String accountNumber;
+    private String cardNumber;
+}
